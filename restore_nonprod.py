11ae1139a98aa0017b4b2533d8e07f48f9311a95
rohith.devu <rohithdevu@rohith.devu>
2019-08-01 04:59:50 -0700

    adding arguments

--- restore_nonprod.py
+++ restore_nonprod.py
@@ -10,8 +10,11 @@ import os
 # from gshell import GnanaShell
 
 # -------------------------------------------------------------> RESTORE starts
-def restore_func():
+def restore_func(stack,surl,tenant,dump_id,passp,worker_pool,uname,key,restore_type,quarter,csv_flag,datasets,
+                 datasets_config,models_list):
     print 'Entered into restore function'
+    print stack,'\n',surl,'\n',tenant,'\n',dump_id,'\n',passp,'\n',worker_pool,'\n',uname,'\n',key,'\n',restore_type,\
+          '\n',quarter,'\n',csv_flag,'\n',datasets,'\n',datasets_config,'\n',models_list
 
 
 
@@ -23,106 +26,58 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Copies only UIP data from a source server to destination",
                                      epilog="Happy Processing and Good Luck.")
 
-    #----------------Forecast Arguments------------------------------
-    parser.add_argument('--forecast', '-f',
-                        type=bool, required=True, default="",
-                        help='Uncheck if you dont want to restore in forecast')
-    parser.add_argument('--forecast_url', '-furl',
-                        type=str, required=False, default="",
-                        help='Destination forcast url to restore.')
-    parser.add_argument('--forecast_tenant','-ft',
-                        type=str, required=True, default="",
-                        help='Tenant name that you want to restore in forecast')
-    parser.add_argument('--funame', '-fu',
-                        type=str, required=False, default="",
-                        help='Username to login as at Forecast Destination server.')
-    parser.add_argument('--fkey', '-fk',
-                        type=str, required=False, default="",
-                        help='idrsa key for the user at Forecast Destination server.')
-    parser.add_argument('--forecast_restore_type','-frt',
-                        type=str, required=False,default="FULL",
-                        help='tells what kind of restore full/partial')
-    parser.add_argument('--csv_data','-csv_flag',
-                        type=str, required=False,default="",
-                        help='List of CSV data seperated by comma if you want to restore only specific csv')
-    parser.add_argument('--f_dump_id','-fdid',
-                        type=str, required=True, default="",
-                        help='Forecast Dump Id')
-    parser.add_argument('--f_passphrase','-fpp',
-                        type=str, required=True, default="",
-                        help='Forecast Dump\'s PassPhrase')
-    parser.add_argument('--forecast_worker_pool', '-fw',
-                        type=str, required=False, default='RESTORE',
-                        help='source forecast server worker pool name')
+    #-------------------------------------Arguments---------------------------------------
 
-    #----------------------ETL Arguments--------------------------------
-    parser.add_argument('--etl', '-e',
-                        type=bool, required=True, default="",
-                        help='Uncheck if you dont want to restore in etl stack')
-    parser.add_argument('--etl_url', '-eurl',
-                        type=str, required=False, default="",
-                        help='Destination etl url to restore.')
-    parser.add_argument('--etl_tenant', '-et',
+    parser.add_argument('--stack', '-s',
                         type=str, required=True, default="",
-                        help='Tenant name that you want to restore in etl')
-    parser.add_argument('--euname', '-eu',
+                        help='Which stack you want to restore Forecast, Etl or Gbm')
+    parser.add_argument('--stack_url', '-surl',
+                        type=str, required=True, default="",
+                        help='Destination url to restore.')
+    parser.add_argument('--tenant', '-t',
+                        type=str, required=True, default="",
+                        help='Tenant name that you want to restore')
+    parser.add_argument('--dump_id', '-did',
+                        type=str, required=True, default="",
+                        help='Id of the Dump that you want to restore')
+    parser.add_argument('--passphrase', '-p',
+                        type=str, required=True, default="",
+                        help='PassPhrase related to the provided dump id')
+    parser.add_argument('--worker_pool', '-w',
+                        type=str, required=False, default='None',
+                        help='Destination stack worker pool name')
+    parser.add_argument('--uname', '-u',
+                        type=str, required=True, default="",
+                        help='Username to login into Destination server.')
+    parser.add_argument('--key', '-k',
                         type=str, required=False, default="",
-                        help='Username to login as at etl Destination server.')
-    parser.add_argument('--ekey', '-ek',
-                        type=str, required=False, default="",
-                        help='idrsa key for the user at ETL Destination server.')
-    parser.add_argument('--etl_restore_type', '-ert',
+                        help='idrsa key for the user to authenticate Destination server.')
+    parser.add_argument('--restore_type', '-r',
                         type=str, required=False, default="FULL",
                         help='tells what kind of restore full/partial')
-    parser.add_argument('--dataset_list', '-dlist',
+    parser.add_argument('--quarter', '-q',
+                        type=str, required=False, default="",
+                        help='Which quarter to restore')
+    parser.add_argument('--csv_flag', '-cf',
+                        type=str, required=False, default="",
+                        help='List of CSV data seperated by comma if you want to restore only specific csv')
+    parser.add_argument('--dataset_list', '-d',
                         type=str, required=False, default="",
                         help='List of datasets seperated by comma if you want to restore only specific data')
-    parser.add_argument('--dataset_config_list', '-dclist',
+    parser.add_argument('--dataset_config_list', '-dc',
                         type=str, required=False, default="",
                         help='List of datasets seperated by comma if you want to restore only specific dataset config')
-    parser.add_argument('--e_dump_id', '-edid',
-                        type=str, required=True, default="",
-                        help='ETL Dump Id')
-    parser.add_argument('--e_passphrase', '-epp',
-                        type=str, required=True, default="",
-                        help='ETL Dump\'s PassPhrase')
-    parser.add_argument('--etl_worker_pool', '-ew',
-                        type=str, required=False, default='',
-                        help='source etl server worker pool name')
-
-    # ----------------------GBM Arguments--------------------------------
-    parser.add_argument('--gbm', '-g',
-                        type=bool, required=True, default="",
-                        help='Uncheck if you dont want to restore in gbm stack')
-    parser.add_argument('--gbm_url', '-gurl',
-                        type=str, required=False, default="",
-                        help='Destination gbm url to restore.')
-    parser.add_argument('--gbm_tenant', '-gt',
-                        type=str, required=True, default="",
-                        help='Tenant name that you want to restore in gbm')
-    parser.add_argument('--guname', '-gu',
-                        type=str, required=False, default="",
-                        help='Username to login as at gbm Destination server.')
-    parser.add_argument('--gkey', '-gk',
-                        type=str, required=False, default="",
-                        help='idrsa key for the user at gbm Destination server.')
-    parser.add_argument('--gbm_restore_type', '-grt',
-                        type=str, required=False, default="FULL",
-                        help='tells what kind of restore full/partial')
-    parser.add_argument('--gbm_dataset_config_list', '-gdclist',
-                        type=str, required=False, default="",
-                        help='List of datasets seperated by comma if you want to restore only specific dataset config')
-    parser.add_argument('--gbm_model_list', '-gmlist',
+    parser.add_argument('--model_list', '-m',
                         type=str, required=False, default="",
                         help='List of models seperated by comma if you want to restore only specific models')
-    parser.add_argument('--g_dump_id', '-gdid',
-                        type=str, required=True, default="",
-                        help='GBM Dump Id')
-    parser.add_argument('--g_passphrase', '-gpp',
-                        type=str, required=True, default="",
-                        help='GBM Dump\'s PassPhrase')
-    parser.add_argument('--gbm_worker_pool', '-gw',
-                        type=str, required=False, default='',
-                        help='source gbm server worker pool name')
 
-    restore_func()
+    args = parser.parse_known_args(sys.argv[1:])
+    other_args = args[1]
+    args = args[0]
+
+    print args
+
+    restore_func(stack=args.stack,surl=args.stack_url,tenant=args.tenant,dump_id=args.dump_id,passp=args.passphrase,
+                 worker_pool=args.worker_pool,uname=args.uname,key=args.key,restore_type=args.restore_type,
+                 quarter=args.quarter,csv_flag=args.csv_flag,datasets=args.dataset_list,
+                 datasets_config=args.dataset_config_list,models_list=args.model_list)
