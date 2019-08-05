0ac40c97870cf17619a34e1abab7e80f68a01af6
rohith.devu <rohithdevu@rohith.devu>
2019-08-05 02:06:45 -0700

    Added Forecast Restore

--- restore_nonprod.py
+++ restore_nonprod.py
@@ -9,16 +9,114 @@ import sys
 import os
 # from gshell import GnanaShell
 
+# ------------------------------------> Login to destination server and Switch to destination tenant
+def destin_tenant_switch(surl,tenant,uname,key):
+
+    # destin_shell = GnanaShell()
+
+    print   '\n##### Loging into destination server: ', surl,uname,key, ' #####'
+    # try:
+    #     destin_shell.server_info(surl)
+    #     os.environ['USERNAME'] = uname
+    #     destin_shell.signin(key=key)
+    # except:
+    #     print '\n##### Unable to singin into the destination server #####'
+    #     exit(1)
+
+    print  '\n ##### Switching to destination tenant: ',tenant,' #####'
+    # try:
+    #     destin_shell.switch_to(tenant)
+    # except:
+    #     print '\n ##### Unable to switch to destination tenant... #####'
+    #     exit(1)
+
+    # return destin_shell
+
+# -------------------------------------------------------------------------> Login and Switch ends
+
+# -----------------------------------------------------------------------> Forecast Restore Starts
+
+def restore_in_forecast(surl,tenant,dump_id,passp,worker_pool,restore_type,quarter,csv_flag):
+    print ' ##### Setting Forecast Restore Parameters #####'
+
+    caches_flag = ['deals']
+
+    if csv_flag in ['ALL','all','All']:
+        csvd = 'ALL'
+    else:
+        csvd = csv_flag.split(',')
+
+    if quarter in ['None','none','NONE']:
+        quarter = None
+    if restore_type in ['FULL','Full','full']:
+        csvd = 'ALL'
+        t_config = True
+
+    elif restore_type == 'Only_Tenant_Config':
+        csvd = []
+        t_config = True
+        caches_flag = []
+    elif restore_type == 'Skip_Config':
+        t_config = False
+    elif restore_type == 'Specific_CSV_Data':
+        t_config = False
+        caches_flag = []
+    elif restore_type == 'Only_Deals_Results':
+        t_config = False
+        csvd = []
+
+    print 'Restore Begins ...'
+    print 'shell.tenant(\'restore\',dump_id=\''+dump_id+'\',pass_phrase=\''+passp+'\',worker_pool=\''+worker_pool+\
+            '\',csv_data=\''+csvd+'\',tenant_config='+str(t_config)+',caches='+str(caches_flag)+',quarter='+str(quarter)+\
+             ',override_inprogress=True)'
+
+    # destin_shell.tenant('restore',dump_id=dump_id,pass_phrase=passp,worker_pool=worker_pool,csv_data=csvd,
+    #                     tenant_config=t_config,caches=caches_flag,quarter=quarter,override_inprogress=True)
+
+
+
+# -------------------------------------------------------------------------> Forecast Restore Ends
+
+# -----------------------------------------------------------------------> ETL Restore Starts
+
+def restore_in_etl(surl,tenant,dump_id,passp,worker_pool,restore_type,datasets,datasets_config):
+    print ' ##### Restore in etl starts #####'
+
+# -------------------------------------------------------------------------> ETL Restore Ends
+
+# -----------------------------------------------------------------------> GBM Restore Starts
+
+def restore_in_gbm(surl,tenant,dump_id,passp,worker_pool,restore_type,quarter,datasets_config,models_list):
+    print ' ##### Restore in gbm starts #####'
+
+# -------------------------------------------------------------------------> GBM Restore Ends
+
+
 # -------------------------------------------------------------> RESTORE starts
 def restore_func(stack,surl,tenant,dump_id,passp,worker_pool,uname,key,restore_type,quarter,csv_flag,datasets,
                  datasets_config,models_list):
     print 'Entered into restore function'
     print stack,'\n',surl,'\n',tenant,'\n',dump_id,'\n',passp,'\n',worker_pool,'\n',uname,'\n',key,'\n',restore_type,\
-          '\n',quarter,'\n',csv_flag,'\n',datasets,'\n',datasets_config,'\n',models_list
+          '\n',quarter,'\n',csv_flag,'\n',datasets,'\n',datasets_config,'\n',models_list,'\n'
+
+
+    ##### Loging into destination server and switching into the destination tenant
+    # dshell = destin_tenant_switch(surl,tenant,uname,key)
+    destin_tenant_switch(surl, tenant, uname, key)
+
+
+    ### Calling appropriate restore function
+    if stack in ['FORECAST','forecast','Forecast']:
+        restore_in_forecast(surl,tenant,dump_id,passp,worker_pool,restore_type,quarter,csv_flag)
+    elif stack in ['ETL','Etl','etl']:
+        restore_in_etl(surl,tenant,dump_id,passp,worker_pool,restore_type,datasets,datasets_config)
+    elif stack in ['GBM','Gbm','gbm']:
+        restore_in_gbm(surl,tenant,dump_id,passp,worker_pool,restore_type,quarter,datasets_config,models_list)
 
 
 
 # ---------------------------------------------------------------------------------> RESTORE ends <|
+
 
 
 if __name__ == '__main__':
@@ -44,7 +142,7 @@ if __name__ == '__main__':
                         type=str, required=True, default="",
                         help='PassPhrase related to the provided dump id')
     parser.add_argument('--worker_pool', '-w',
-                        type=str, required=False, default='None',
+                        type=str, required=False, default=None,
                         help='Destination stack worker pool name')
     parser.add_argument('--uname', '-u',
                         type=str, required=True, default="",
