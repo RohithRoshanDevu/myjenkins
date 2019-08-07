a87229e78521e069ca610b7c0ef6cf0f9aae7d69
rohith.devu <rohithdevu@rohith.devu>
2019-08-06 23:02:28 -0700

    Added ETL and GBM

--- restore_nonprod.py
+++ restore_nonprod.py
@@ -23,7 +23,7 @@ def destin_tenant_switch(surl,tenant,uname,key):
     #     print '\n##### Unable to singin into the destination server #####'
     #     exit(1)
 
-    print  '\n ##### Switching to destination tenant: ',tenant,' #####'
+    print  '\n##### Switching to destination tenant: ',tenant,' #####'
     # try:
     #     destin_shell.switch_to(tenant)
     # except:
@@ -36,8 +36,8 @@ def destin_tenant_switch(surl,tenant,uname,key):
 
 # -----------------------------------------------------------------------> Forecast Restore Starts
 
-def restore_in_forecast(surl,tenant,dump_id,passp,worker_pool,restore_type,quarter,csv_flag):
-    print ' ##### Setting Forecast Restore Parameters #####'
+def restore_in_forecast(surl,tenant,dump_id,passp,worker_pool,restore_type,q_flag,csv_flag):
+    print '##### Setting Forecast Restore Parameters #####'
 
     caches_flag = ['deals']
 
@@ -46,32 +46,32 @@ def restore_in_forecast(surl,tenant,dump_id,passp,worker_pool,restore_type,quart
     else:
         csvd = csv_flag.split(',')
 
-    if quarter in ['None','none','NONE']:
-        quarter = None
+    if q_flag in ['None','none','NONE']:
+        q_flag = None
     if restore_type in ['FULL','Full','full']:
         csvd = 'ALL'
-        t_config = True
+        tc_flag = True
 
     elif restore_type == 'Only_Tenant_Config':
         csvd = []
-        t_config = True
+        tc_flag = True
         caches_flag = []
     elif restore_type == 'Skip_Config':
-        t_config = False
+        tc_flag = False
     elif restore_type == 'Specific_CSV_Data':
-        t_config = False
+        tc_flag = False
         caches_flag = []
     elif restore_type == 'Only_Deals_Results':
-        t_config = False
+        tc_flag = False
         csvd = []
 
     print 'Restore Begins ...'
-    print 'shell.tenant(\'restore\',dump_id=\''+dump_id+'\',pass_phrase=\''+passp+'\',worker_pool=\''+worker_pool+\
-            '\',csv_data=\''+csvd+'\',tenant_config='+str(t_config)+',caches='+str(caches_flag)+',quarter='+str(quarter)+\
+    print 'shell.tenant(\'restore\',dump_id=\''+dump_id+'\',pass_phrase=\''+passp+'\',worker_pool='+worker_pool+\
+            ',csv_data=\''+csvd+'\',tenant_config='+str(tc_flag)+',caches='+str(caches_flag)+',quarter='+str(q_flag)+\
              ',override_inprogress=True)'
 
     # destin_shell.tenant('restore',dump_id=dump_id,pass_phrase=passp,worker_pool=worker_pool,csv_data=csvd,
-    #                     tenant_config=t_config,caches=caches_flag,quarter=quarter,override_inprogress=True)
+    #                     tenant_config=tc_flag,caches=caches_flag,quarter=q_flag,override_inprogress=True)
 
 
 
@@ -79,25 +79,133 @@ def restore_in_forecast(surl,tenant,dump_id,passp,worker_pool,restore_type,quart
 
 # -----------------------------------------------------------------------> ETL Restore Starts
 
-def restore_in_etl(surl,tenant,dump_id,passp,worker_pool,restore_type,datasets,datasets_config):
-    print ' ##### Restore in etl starts #####'
+def restore_in_etl(surl,tenant,did,passp,workerp,restore_type,dsets):
+    print ' ##### Setting ETL Restore Parameters #####'
+
+    if workerp in ["None", 'none', 'None']:
+        workerp = None
+
+    if dsets in ['ALL','All','all']:
+        d_flag = 'ALL'
+    else:
+        d_flag = dsets.split(',')
+
+    if restore_type in ['Only_Config','Only_Tenant_Config','Only_Dataset_Config','Specific_Dataset_Config']:
+        c_flag = True
+    if restore_type in ['Only_Config','Only_Dataset_Config','Skip_Config']:
+        d_flag = 'ALL'
+    if restore_type in ['Only_Config','Only_Tenant_Config']:
+        tc_flag = True
+    if restore_type in ['Only_Dataset_Config','Specific_Dataset_Config']:
+        tc_flag = False
+    if restore_type in ['Skip_Config','Specific_UIP_Data']:
+        skip_flag = True
+    if restore_type == 'Only_Tenant_Config':
+        d_flag = []
+
+    if restore_type in ['FULL','Full','full']:
+            print 'shell.tenant(\'restore\',dump_id=\'' + did + '\',pass_phrase=\'' + passp + '\',worker_pool=' + \
+               str(workerp) + ',override_inprogress=True)'
+
+        # destin_shell.tenant('restore, dump_id=did, pass_phrase=passp, worker_pool=workerp, override_inprogress=True)
+
+    elif restore_type in ['Only_Config','Only_Tenant_Config','Only_Dataset_Config','Specific_Dataset_Config']:
+        print 'shell.tenant(\'restore\',dump_id=\'' + did + '\',pass_phrase=\'' + passp + '\',worker_pool=' + \
+              str(workerp) + ',datasets='+str(d_flag)+',tenant_config='+str(tc_flag)+\
+              ',config_only='+str(c_flag)+',override_inprogress=True)'
+
+        # destin_shell.tenant('restore', dump_id=did,pass_phrase=passp,worker_pool=workerp,
+        #                     datasets=d_flag, tenant_config=tc_flag,config_only=c_flag,override_inprogress=True)
+
+    elif restore_type in ['Skip_Config','Specific_UIP_Data']:
+        print 'shell.tenant(\'restore\',dump_id=\'' + did + '\',pass_phrase=\'' + passp + '\',worker_pool=' + \
+              str(workerp) + ',datasets=' + str(d_flag) + ',skip_config=' + str(skip_flag) + ',override_inprogress=True)'
+
+        # destin_shell.tenant('restore',dump_id=did,pass_phrase=passp,worker_pool=workerp,datasets=d_flag,
+        #                     skip_config=skip_flag,override_inprogress=True)
 
 # -------------------------------------------------------------------------> ETL Restore Ends
 
 # -----------------------------------------------------------------------> GBM Restore Starts
 
-def restore_in_gbm(surl,tenant,dump_id,passp,worker_pool,restore_type,quarter,datasets_config,models_list):
+def restore_in_gbm(surl,tenant,did,passp,workerp,restore_type,q_flag,dsets,models_list):
     print ' ##### Restore in gbm starts #####'
+
+    if workerp in ["None", 'none', 'None']:
+        workerp = None
+
+    if dsets in ['ALL','All','all']:
+        d_flag = 'ALL'
+    else:
+        d_flag = dsets.split(',')
+
+    if models_list in ['ALL', 'All', 'all']:
+        m_flag = 'ALL'
+    else:
+        m_flag = models_list.split(',')
+
+
+    if q_flag in ['None','none','NONE']:
+        q_flag = None
+
+    if restore_type in ['Only_Config', 'Only_Tenant_Config', 'Only_TargetSpec_Config', 'Only_Dataset_Config']:
+        c_flag = True
+    if restore_type in ['Only_Config', 'Only_Tenant_Config']:
+        tc_flag = True
+    if restore_type in ['Only_TargetSpec_Config', 'Only_Dataset_Config']:
+        tc_flag = False
+    if restore_type in ['Only_Config', 'Only_Dataset_Config', 'Skip_Config', 'Only_Models', 'Specific_Model']:
+        d_flag = 'ALL'
+    if restore_type in ['Only_Config', 'Only_TargetSpec_Config', 'Skip_Config', 'Only_Combined_Results']:
+        combined_flag = 'ALL'
+    if restore_type in ['Only_Tenant_Config', 'Only_Dataset_Config', 'Only_Models', 'Specific_Model']:
+        combined_flag = []
+    if restore_type in ['Only_Tenant_Config', 'Only_TargetSpec_Config', 'Only_Combined_Results']
+        d_flag = []
+    if restore_type in ['Only_Config', 'Skip_Config', 'Only_Models']:
+        m_flag = 'ALL'
+    if restore_type in ['Only_Tenant_Config', 'Only_TargetSpec_Config', 'Only_Dataset_Config', 'Only_Combined_Results']:
+        m_flag = []
+    if restore_type in ['Skip_Config', 'Only_Models', 'Specific_Model', 'Only_Combined_Results']:
+        skip_flag = True
+
+    if restore_type in ['FULL','Full','full']:
+        print 'shell.tenant(\'restore\',dump_id=\'' + did + '\',pass_phrase=\'' + passp + '\',worker_pool=' + \
+               str(workerp) + ',override_inprogress=True)'
+
+        # destin_shell.tenant('restore',dump_id=dump_id,pass_phrase=passp,worker_pool=worker_pool,
+        #                     override_inprogress=True)
+
+    elif restore_type in ['Only_Config','Only_Tenant_Config','Only_Dataset_Config','Only_TargetSpec_Config']:
+        print 'shell.tenant(\'restore\',dump_id=\'' + did + '\',pass_phrase=\'' + passp + '\',worker_pool=' + \
+              str(workerp) + ',datasets='+str(d_flag)+',tenant_config='+str(tc_flag)+\
+              ',config_only='+str(c_flag)+'combined_results='+str(combined_flag)+\
+              'model='+str(m_flag)+'quarter='+q_flag+',override_inprogress=True)'
+
+        # destin_shell.tenant('restore', dump_id=did, pass_phrase=passp, worker_pool=workerp,
+        #                     datasets=d_flag, tenant_config=tc_flag, config_only=c_flag,
+        #                     combined_results=combined_flag, model=m_flag,
+        #                     quarter=q_flag, override_inprogress=True)
+
+    elif restore_type in ['Skip_Config','Only_Models','Specific_Model','Only_Combined_Results']:
+        print 'shell.tenant(\'restore\',dump_id=\'' + did + '\',pass_phrase=\'' + passp + '\',worker_pool=' + \
+              str(workerp) + ',datasets=' + str(d_flag) + 'combined_results='+str(combined_flag)+\
+              'model='+str(m_flag)+',skip_config=' + str(skip_flag) + \
+              'quarter='+str(q_flag)+',override_inprogress=True)'
+
+        # destin_shell.tenant('restore',dump_id=did,pass_phrase=passp,worker_pool=workerp,datasets=d_flag,
+        #                     combined_results=combined_flag, model=m_flag, skip_config=skip_flag,
+        #                     quarter=q_flag, override_inprogress=True)
 
 # -------------------------------------------------------------------------> GBM Restore Ends
 
 
 # -------------------------------------------------------------> RESTORE starts
-def restore_func(stack,surl,tenant,dump_id,passp,worker_pool,uname,key,restore_type,quarter,csv_flag,datasets,
-                 datasets_config,models_list):
+def restore_func(stack,surl,tenant,did,passp,workerp,uname,key,restore_type,q_flag,csv_flag,dsets,
+                 dsets_config,models_list):
     print 'Entered into restore function'
-    print stack,'\n',surl,'\n',tenant,'\n',dump_id,'\n',passp,'\n',worker_pool,'\n',uname,'\n',key,'\n',restore_type,\
-          '\n',quarter,'\n',csv_flag,'\n',datasets,'\n',datasets_config,'\n',models_list,'\n'
+    print stack,'\n',surl,'\n',tenant,'\n',did,'\n',passp,'\n',workerp,'\n',uname,'\n',key,'\n',restore_type,\
+          '\n',q_flag,'\n',csv_flag,'\n',dsets,'\n',dsets_config,'\n',models_list,'\n'
 
 
     ##### Loging into destination server and switching into the destination tenant
@@ -107,11 +215,11 @@ def restore_func(stack,surl,tenant,dump_id,passp,worker_pool,uname,key,restore_t
 
     ### Calling appropriate restore function
     if stack in ['FORECAST','forecast','Forecast']:
-        restore_in_forecast(surl,tenant,dump_id,passp,worker_pool,restore_type,quarter,csv_flag)
+        restore_in_forecast(surl,tenant,did,passp,workerp,restore_type,q_flag,csv_flag)
     elif stack in ['ETL','Etl','etl']:
-        restore_in_etl(surl,tenant,dump_id,passp,worker_pool,restore_type,datasets,datasets_config)
+        restore_in_etl(surl,tenant,did,passp,workerp,restore_type,dsets)
     elif stack in ['GBM','Gbm','gbm']:
-        restore_in_gbm(surl,tenant,dump_id,passp,worker_pool,restore_type,quarter,datasets_config,models_list)
+        restore_in_gbm(surl,tenant,did,passp,workerp,restore_type,q_flag,dsets,models_list)
 
 
 
@@ -162,9 +270,6 @@ if __name__ == '__main__':
     parser.add_argument('--dataset_list', '-d',
                         type=str, required=False, default="",
                         help='List of datasets seperated by comma if you want to restore only specific data')
-    parser.add_argument('--dataset_config_list', '-dc',
-                        type=str, required=False, default="",
-                        help='List of datasets seperated by comma if you want to restore only specific dataset config')
     parser.add_argument('--model_list', '-m',
                         type=str, required=False, default="",
                         help='List of models seperated by comma if you want to restore only specific models')
@@ -175,7 +280,6 @@ if __name__ == '__main__':
 
     print args
 
-    restore_func(stack=args.stack,surl=args.stack_url,tenant=args.tenant,dump_id=args.dump_id,passp=args.passphrase,
-                 worker_pool=args.worker_pool,uname=args.uname,key=args.key,restore_type=args.restore_type,
-                 quarter=args.quarter,csv_flag=args.csv_flag,datasets=args.dataset_list,
-                 datasets_config=args.dataset_config_list,models_list=args.model_list)
+    restore_func(stack=args.stack,surl=args.stack_url,tenant=args.tenant,did=args.dump_id,passp=args.passphrase,
+                 workerp=args.worker_pool,uname=args.uname,key=args.key,restore_type=args.restore_type,
+                 q_flag=args.quarter,csv_flag=args.csv_flag,dsets=args.dataset_list,models_list=args.model_list)
