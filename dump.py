b32630c1cf4f02e805728420101ff5f5ed938e8f
rohith.devu <rohithdevu@rohith.devu>
2019-09-17 01:08:43 -0700

    correct file path

--- dump.py
+++ dump.py
@@ -3,12 +3,12 @@ import csv
 
 print 'Writing into the file : \n'
 
-with file('/dumps/dump_info.csv','a') as fp:
+with file('/var/jenkins-home/dumps/dump_info.csv','a') as fp:
 	csv_writer = csv.writer(fp,delimiter=',')
 	csv_writer.writerow('20190917','delphix.com','config','ffffhhfhfjksfks','passphrase',) 
 
 print 'Reading from the file : \n'
-with file('/dumps/dump_info.csv','r') as fp2:
+with file('/var/jenkins-home/dumps/dump_info.csv','r') as fp2:
 	for line in fp2.read().splitlines():
 		print line
 
