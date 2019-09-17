9aaadf2ac05d1be3d2d0a75a68aa4217c6ab5ee9
rohith.devu <rohithdevu@rohith.devu>
2019-09-17 00:11:21 -0700

    adding read code

--- dump.py
+++ dump.py
@@ -1,9 +1,14 @@
 import os
 import csv
 
-with file('/dumps/dump_info.csv,'a') as fp:
+print 'Writing into the file : \n'
+
+with file('/dumps/dump_info.csv','a') as fp:
 	csv_writer = csv.writer(fp,delimiter=',')
 	csv_writer.writerow('20190917','delphix.com','config','ffffhhfhfjksfks','passphrase',) 
 
-
+print 'Reading from the file : \n'
+with file('/dumps/dump_info.csv','r') as fp2:
+	for line in fp2.read().splitlines():
+		print line
 
