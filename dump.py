b0df4c87328423138fd7a822800bd7d0524c08db
rohith.devu <rohithdevu@rohith.devu>
2019-09-17 01:47:04 -0700

    filename

--- dump.py
+++ dump.py
@@ -3,12 +3,15 @@ import csv
 
 print 'Writing into the file : \n'
 
-with file('/var/jenkins-home/dumps/dump_info.csv','a') as fp:
+filename = 'dump_info.csv'
+
+
+with file(filename,'a') as fp:
 	csv_writer = csv.writer(fp,delimiter=',')
 	csv_writer.writerow('20190917','delphix.com','config','ffffhhfhfjksfks','passphrase',) 
 
 print 'Reading from the file : \n'
-with file('/var/jenkins-home/dumps/dump_info.csv','r') as fp2:
+with file(filename,'r') as fp2:
 	for line in fp2.read().splitlines():
 		print line
 
